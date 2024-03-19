7638ecec6081633a2f277b386bbc34bb753aed1e
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-19 17:12:24 +0330

    delete and complete actions created

--- src/components/Todos/TodoItem.jsx
+++ src/components/Todos/TodoItem.jsx
@@ -1,12 +1,21 @@
+import { useDispatch } from "react-redux";
+import { deleteTodo, actionTodo } from "../../features/todo/todoSlice";
+
 const TodoItem = ({ id, title, complete }) => {
+  const dispatch = useDispatch();
   return (
     <li className={`list-group-item ${complete && "list-group-item-success"}`}>
       <div className="d-flex justify-content-between">
         <span className="d-flex align-items-center gap-1">
-          <input type="checkbox" className="mr-3" checked={complete}></input>
+          <input type="checkbox" className="mr-3" checked={complete} onChange={()=>dispatch(actionTodo({id}))}></input>
           <span>{title}</span>
         </span>
-        <button className="btn btn-danger">Delete</button>
+        <button
+          className="btn btn-danger"
+          onClick={() => dispatch(deleteTodo({ id }))}
+        >
+          Delete
+        </button>
       </div>
     </li>
   );
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -15,8 +15,17 @@ const todoSlice = createSlice({
       };
       state.todos.push(newTodo);
     },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id != action.payload.id);
+    },
+    actionTodo: (state, action) => {
+      const selectedId = state.todos.find(
+        (todo) => todo.id == action.payload.id
+      );
+      selectedId.complete = !selectedId.complete;
+    },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, deleteTodo, actionTodo } = todoSlice.actions;
 export default todoSlice.reducer;
