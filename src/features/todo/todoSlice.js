1df9a52f4b53218020d1a949c69465f5d3e85c6a
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-20 22:32:09 +0330

    getAsyncTodo created to get data from server

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,8 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   todos: [],
+  loading: false,
+  error: "",
 };
+axios.defaults.baseURL = "http://localhost:5000";
+
+export const getAsyncTodo = createAsyncThunk(
+  "todo/getAsyncTodo",
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get("/todos");
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 const todoSlice = createSlice({
   name: "todos",
   initialState,
@@ -24,6 +41,21 @@ const todoSlice = createSlice({
       );
       selectedId.complete = !selectedId.complete;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getAsyncTodo.pending, (state, action) => {
+      state.loading = true;
+    }),
+      builder.addCase(getAsyncTodo.fulfilled, (state, action) => {
+        state.loading = false;
+        state.todos = action.payload;
+        state.error = "";
+      }),
+      builder.addCase(getAsyncTodo.rejected, (state, action) => {
+        state.loading = false;
+        state.todos = [];
+        state.error = action.payload;
+      });
   },
 });
 
